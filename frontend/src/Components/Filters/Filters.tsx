b731864dbd2197b87aeb9af87c42687a6c0dffc7
Breno Nascimento <satsukebare23@gmail.com>
2022-10-18 18:16:29 -0300

    remove select filter in filter component

--- frontend/src/Components/Filters/Filters.tsx
+++ frontend/src/Components/Filters/Filters.tsx
@@ -9,12 +9,6 @@ function Filters() {
   const [addNewFilter, setAddNewFilter] = useState(false);
   const dispatch = useDispatch();
   const filters = Object.keys(useSelector(({ herosModules }:any) => herosModules.filters));
-  const currFilter = useSelector(({ herosModules }:any) => herosModules.currFilter);
-
-  const selectFilter = (filter: string) => {
-    if (filter === currFilter) return dispatch(selectCurrFilter(''));
-    dispatch(selectCurrFilter(filter));
-  };
 
   const newFilter = () => {
     if (!newFilterName) return alert('Por favor Digite alguma coisa');
