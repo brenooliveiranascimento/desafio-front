35b2451d0ff43d9c1c137ae68f506a41f987936c
Breno Nascimento <satsukebare23@gmail.com>
2022-10-18 17:04:52 -0300

    add store and root reducer

--- frontend/src/Components/Filters/Filters.tsx
+++ frontend/src/Components/Filters/Filters.tsx
@@ -4,7 +4,11 @@ import './filters.css';
 function Filters() {
   return (
     <section className="filter_container">
-      <h1>widjwd</h1>
+      <h1>Marvel</h1>
+      <h1>DC</h1>
+      <h1>Viões</h1>
+      <h1>Herois</h1>
+      <h1>Todos</h1>
     </section>
   );
 }
