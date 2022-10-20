c9554d1a1bfd2b1d65d534448427bd40aae348a9
Breno Nascimento <satsukebare23@gmail.com>
2022-10-20 13:58:52 -0300

    best align

--- frontend/src/Components/CardsArea/CardsArea.tsx
+++ frontend/src/Components/CardsArea/CardsArea.tsx
@@ -11,7 +11,7 @@ function CardsArea() {
   const herosInf: heroModulesTypes = useSelector(
     ({ herosModules }: heroModulesTypes) => herosModules,
   );
-  const { currFilter, charter, load } = herosInf;
+  const { currList, charter, load } = herosInf;
 
   const fetchCharter = async () => {
     dispatch(requestCharters());
@@ -23,21 +23,21 @@ function CardsArea() {
 
   useEffect(() => {
     fetchCharter();
-  }, [currFilter]);
+  }, [currList]);
 
   return (
     <section className="cardsArea_container">
       {
-        load && currFilter !== 'All' ? 'Loading' : (
+        load ? 'Loading' : (
           <section className="card_area">
             { charter.length ? charter.map((currCharter: heroTypes) => (
               <Cards key={currCharter.id} currCharter={currCharter} />
             ))
-              : <h1>{currFilter !== 'All' && 'Nenhum personagem encontrado'}</h1> }
+              : <h1>{currList !== 'All' && 'Nenhum personagem encontrado'}</h1> }
           </section>
         )
       }
-      {currFilter === 'All' && (
+      {currList === 'All' && (
       <button onClick={loadMore} type="button">
         {load ? 'Loading...' : 'Load More'}
       </button>
