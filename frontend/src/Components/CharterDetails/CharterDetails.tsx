facb7d6137cb231910e572cd2bef56ddb5804f8e
Breno Nascimento <satsukebare23@gmail.com>
2022-10-21 14:06:38 -0300

    consume charter store in detail component

--- frontend/src/Components/CharterDetails/CharterDetails.tsx
+++ frontend/src/Components/CharterDetails/CharterDetails.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { heroTypes } from '../../types/heroTypes';
+import { useSelector } from 'react-redux';
+import { heroDetailsStateType, heroTypes } from '../../types/heroTypes';
 import './CharterDetails.css';
 
+interface detailStoreType {
+  heroDetail: heroDetailsStateType
+}
+
 function CharterDetails() {
+  const { charter } = useSelector(({ heroDetail }: detailStoreType) => heroDetail);
+  console.log(charter);
+
   return (
     <div className="charterDetails_container">
       <h1>djawiodjwio</h1>
